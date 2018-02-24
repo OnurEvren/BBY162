c739eac1e69caab2acba41fbf4350ee8aa848994
Onur Evren <36562926+OnurEvren@users.noreply.github.com>
2018-02-25 02:35:49 +0300

    Update uygulama02.py

--- uygulama02.py
+++ uygulama02.py
@@ -4,12 +4,12 @@ girdi = int(input("Saniye cinsinden bir değer giriniz...:"))
 print("-"*53)
 print("")
 
-yil = girdi / 31536000
-ay = (girdi % 31536000) / 2628000
-gun = ((girdi % 31536000) % 2628000) / 86400
-saat = (((girdi% 31536000) % 2628000) % 86400) / 3600
-dakika = ((((girdi % 31536000) % 2628000) % 86400)% 3600) / 60
-saniye = (((((girdi % 31536000) % 2628000) % 86400) % 3600) % 60)
+yil = girdi / 31557600
+ay = (girdi % 31557600) / 2629800
+gun = ((girdi % 31557600) % 2629800) / 86400
+saat = (((girdi% 31557600) % 2629800) % 86400) / 3600
+dakika = ((((girdi % 31557600) % 2629800) % 86400)% 3600) / 60
+saniye = (((((girdi % 31557600) % 2629800) % 86400) % 3600) % 60)
 
 
 print(int(yil) , "Yıl" , "," , int(ay) ,"Ay" , "," , int(gun) ,"Gün" , "," , int(saat) , "Saat" , "," , int(dakika)  ,"Dakika", "," , int(saniye) , "Saniye")
