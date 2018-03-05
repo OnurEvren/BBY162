1a7e1b3be5ec8f02ad2396c819820a47d3b909af
Onur Evren <36562926+OnurEvren@users.noreply.github.com>
2018-03-05 13:48:57 +0300

    Update uygulama02.py

--- uygulama02.py
+++ uygulama02.py
@@ -11,6 +11,12 @@ saat = (((girdi% 31557600) % 2629800) % 86400) / 3600
 dakika = ((((girdi % 31557600) % 2629800) % 86400)% 3600) / 60
 saniye = (((((girdi % 31557600) % 2629800) % 86400) % 3600) % 60)
 
+"""
+1 yıl  = 365 gün 6 saat
+1 ay   = 30 gün olarak hesaplanmıştır.
+
+"""
+
 
 print(int(yil) , "Yıl" , "," , int(ay) ,"Ay" , "," , int(gun) ,"Gün" , "," , int(saat) , "Saat" , "," , int(dakika)  ,"Dakika", "," , int(saniye) , "Saniye")
 print("")
